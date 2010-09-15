4e60b22470d416fe6af597a4b6bebe1e21762de8
nybon <ni2yue4@gmail.com>
2010-09-15 22:14:45 +0800

    Clean up puzzle solving code.

--- src/main/java/com/niyue/fling/FlingPuzzleSolver.java
+++ src/main/java/com/niyue/fling/FlingPuzzleSolver.java
@@ -4,70 +4,54 @@ import java.util.Stack;
 import static com.niyue.fling.Direction.*;
 
 public class FlingPuzzleSolver {
+	private static Stack<Movement> NO_SOLUTION = null;
 	public Stack<Movement> solve(Board board) {
-		Stack<Movement> solution = new Stack<Movement>();
-		if(!board.isSolved()) {
+		Stack<Movement> solution = NO_SOLUTION;
+		if(board.isSolved()) {
+			solution = new Stack<Movement>();
+		}
+		else {
 			for(int x=0;x<Board.DEFAULT_WIDTH;x++) {
 				for(int y=0;y<Board.DEFAULT_HEIGHT;y++) {
-					if(board.isMovable(x, y, UP)) {
-						Board beforeMoveBoard = new Board(board);
-						board.move(x, y, UP);
-						Stack<Movement> subSolution = solve(board);
-						if(subSolution != null) {
-							Movement movement = new Movement(x, y, UP);
-							solution.push(movement);
-							solution.addAll(subSolution);
-							return solution;
-						}
-						else {
-							board = beforeMoveBoard;
-						}
-					}
-					if(board.isMovable(x, y, DOWN)) {
-						Board beforeMoveBoard = new Board(board);
-						board.move(x, y, DOWN);
-						Stack<Movement> subSolution = solve(board);
-						if(subSolution != null) {
-							Movement movement = new Movement(x, y, DOWN);
-							solution.push(movement);
-							solution.addAll(subSolution);
-							return solution;
-						}
-						else {
-							board = beforeMoveBoard;
-						}
-					}
-					if(board.isMovable(x, y, LEFT)) {
-						Board beforeMoveBoard = new Board(board);
-						board.move(x, y, LEFT);
-						Stack<Movement> subSolution = solve(board);
-						if(subSolution != null) {
-							Movement movement = new Movement(x, y, LEFT);
-							solution.push(movement);
-							solution.addAll(subSolution);
-							return solution;
-						}
-						else {
-							board = beforeMoveBoard;
-						}
-					}
-					if(board.isMovable(x, y, RIGHT)) {
-						Board beforeMoveBoard = new Board(board);
-						board.move(x, y, RIGHT);
-						Stack<Movement> subSolution = solve(board);
-						if(subSolution != null) {
-							Movement movement = new Movement(x, y, RIGHT);
-							solution.push(movement);
-							solution.addAll(subSolution);
-							return solution;
-						}
-						else {
-							board = beforeMoveBoard;
-						}
-					}
+					solution = tryMove(x, y, UP, board);
+					if(foundSolution(solution))
+						break;
+					
+					solution = tryMove(x, y, DOWN, board);
+					if(foundSolution(solution))
+						break;
+					
+					solution = tryMove(x, y, LEFT, board);
+					if(foundSolution(solution))
+						break;
+					
+					solution = tryMove(x, y, RIGHT, board);
+					if(foundSolution(solution))
+						break;
 				}
+				if(foundSolution(solution))
+					break;
 			}
-			solution = null;
+		}
+		return solution;
+	}
+	
+	private boolean foundSolution(Stack<Movement> solution) {
+		return solution != NO_SOLUTION;
+	}
+	
+	private Stack<Movement> tryMove(int x, int y, Direction direction, Board board) {
+		Stack<Movement> solution = NO_SOLUTION;
+		if(board.isMovable(x, y, direction)) {
+			Board newBoard = new Board(board);
+			newBoard.move(x, y, direction);
+			Stack<Movement> subSolution = solve(newBoard);
+			if(foundSolution(subSolution)) {
+				Movement movement = new Movement(x, y, direction);
+				solution = new Stack<Movement>();
+				solution.push(movement);
+				solution.addAll(subSolution);
+			}
 		}
 		return solution;
 	}
